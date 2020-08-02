673ee50ebcacb75ed29ed29362ced4238a389569
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:43:41 -0400

    Conditional structure examples and exercises

---
+++ Conditional_Structure_Exercise_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EstruturaCondicionalExercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero<0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+        }
+    }
+}
---
+++ Conditional_Structure_Exercise_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaCondicionalExercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números inteiros:");
+            String[] vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+
+            if (a % b == 0 || b%a == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são Multiplos");
+            }
+        }
+    }
+}
