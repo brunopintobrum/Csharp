33e329c800b58b2e1b5db7528e1a099043b93b9a
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:21:27 -0400

    Inverse name using StringBuilder

---
+++ Inverse_Name_With_StringBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Inverse_Name_With_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            string nameReverse = new string(name.Reverse().ToArray());
+
+            Console.WriteLine($"Inverted name: {nameReverse}");
+
+        }
+    }
+}
