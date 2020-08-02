9ece0a577078c261bffffe7dc994eb04ed6c1ad3
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:53:19 -0400

    POO - Calculatrice retangulo, area, perimetro e diagonal

---
+++ POO_Calculatrice_RetanguloAreaPerimetroDiagonal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace POORetanguloAreaPerimetroDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            retangulo.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+}
---
+++ POO_Calculatrice_RetanguloAreaPerimetroDiagonal/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POORetanguloAreaPerimetroDiagonal
+{
+    class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+        public double Area()
+        {
+            return largura * altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (largura + altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(largura * largura + altura * altura);
+        }
+    }
+    
+}
