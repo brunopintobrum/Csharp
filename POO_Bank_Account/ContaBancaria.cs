708adde83ff1fe9f8d22fc37d5299b6a9b7d1a2e
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:04:10 -0400

    Example using bank account - POO

---
+++ POO_Bank_Account/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace POOContaBancaria
+{
+    class ContaBancaria
+    {
+
+        public int Numero { get; private set; } //numero da conta nao pode ser alterado
+        public string Titular { get; set; }
+        public double Saldo { get; private set; } //o saldo pode mudar mas precisa ser protegido
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
