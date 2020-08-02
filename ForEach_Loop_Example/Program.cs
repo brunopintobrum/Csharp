46027a42e8d2a7d79eee4172402d4cd0c57d8bf9
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:08:02 -0400

    Example using FOREACH LOOP

---
+++ ForEach_Loop_Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LaçoForEach {
+    class Program {
+        static void Main(string[] args) {
+
+            //=================================================================================
+            //                              USANDO O FOREACH
+            //=================================================================================
+
+            string[] vect = new string[] { "Maria", "Bob", "Alex" };
+
+            foreach(string obj in vect) { // para cada objeto 'obj' contido em vect, faça!
+                Console.WriteLine(obj);
+            }
+
+            //=================================================================================
+            //                              USANDO O FOR
+            //=================================================================================
+
+            string[] vect2 = new string[] { "Bruno", "Fernanda" };
+
+            for(int i = 0; i < vect2.Length; i++) {
+                Console.WriteLine(vect2[i]);
+            }
+
+        }
+    }
+}
