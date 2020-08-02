1f22e5f8990c932fb16f426e6a8eb61417c0fc75
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:13:42 -0400

    Exercices Set Conjuntos

---
+++ Set_Conjuntos_Excercise_01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConjuntosExercicios01 {
+    class Program {
+        static void Main(string[] args) {
+
+            //Declaração e instanciação de Conjuntos
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+
+            // adiciona itens no conjunto
+            A.Add(3);
+            A.Add(5);
+            A.Add(8);
+            A.Add(9);
+
+            B.Add(3);
+            B.Add(4);
+            B.Add(5);
+
+
+            Console.WriteLine("Conjunto A:");
+
+            //percorre o conjunto, não podemos fazer com o FOR normal.
+            foreach (int x in A) { //para cada inteiro x dentro do conjunto A
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Conjunto B:");
+            foreach (int x in B) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("Digite um valor inteiro: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (B.Contains(N)) {
+                Console.WriteLine(N + " pertence ao conjunto B");
+            }
+            else {
+                Console.WriteLine(N + " não pertence ao conjunto B");
+            }
+
+            Console.WriteLine("=====================");
+            // diferença entre conjuntos
+            A.ExceptWith(B);
+            foreach (int x in A) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("=====================");
+            // faz uma uniao entre conjuntos
+            A.UnionWith(B);
+            foreach (int x in A) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("=====================");
+            // faz uma interseção entre conjuntos
+            A.IntersectWith(B);
+            foreach (int x in A) {
+                Console.WriteLine(x);
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Set_Conjuntos_Excercise_02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConjuntosExercicio02 {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            Console.Write("O curso A possui quantos alunos? ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso A: ");
+            for (int i = 0; i < N; i++) {
+                int x = int.Parse(Console.ReadLine());
+                A.Add(x);
+            }
+
+            Console.Write("O curso B possui quantos alunos? ");
+            N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso B: ");
+            for (int i = 0; i < N; i++) {
+                int x = int.Parse(Console.ReadLine());
+                B.Add(x);
+            }
+
+            Console.Write("O curso C possui quantos alunos? ");
+            N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso C: ");
+            for (int i = 0; i < N; i++) {
+                int x = int.Parse(Console.ReadLine());
+                C.Add(x);
+            }
+
+            HashSet<int> novo = new HashSet<int>();
+            novo.UnionWith(A);
+            novo.UnionWith(B);
+            novo.UnionWith(C);
+            Console.WriteLine("Total de Alunos: " + novo.Count);
+            Console.ReadLine();
+
+
+        }
+    }
+}
