b04028af6bc4dac2af01b5385d828626d8c347c4
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:44:17 -0400

    Scope and Initialization

---
+++ Scope_e_Initialization/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Escopo_E_Inicializacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;//escopo: nao pode inicializar a variavel dentro do if
+            if (preco > 100.0)
+            {
+                desconto = preco * 0.1;//nao poderia inicializar aqui a variavel
+            }
+
+            Console.WriteLine(desconto);//daria erro aqui!
+        }
+    }
+}
