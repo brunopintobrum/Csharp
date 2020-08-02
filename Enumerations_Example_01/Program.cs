d861642cb5175502d28214af436a388b24d0b6a4
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:20:28 -0400

    Enumeration example

---
+++ Enumerations_Example_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using Enumerations_Example_01.Entities;
+using Enumerations_Example_01.Entities.Enums;
+using System;
+
+namespace Enumerations_Example_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            
+            // Converting format Enum --> String
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            // Conversion inverse string --> enum
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+            
+
+        }
+    }
+}
