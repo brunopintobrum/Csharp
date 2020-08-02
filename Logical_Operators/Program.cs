20d5336a3df253809a8a72e4e81141271e27f79f
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:42:27 -0400

    Logical operators

---
+++ Logical_Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 2 > 3 && 4 != 5;
+
+            Console.WriteLine(c1);
+
+            bool c2 = 2 > 3 || 4 != 5;
+
+            Console.WriteLine(c2);
+
+            bool c3 = !(2 > 3) && 4 != 5;
+
+            Console.WriteLine(c3);
+            Console.WriteLine("-------------------");
+
+        }
+    }
+}
