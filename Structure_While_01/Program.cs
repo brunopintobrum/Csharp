e70f5146ca29fb0c8c2484f033876b97f7ea68a2
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:46:14 -0400

    Structure While examples

---
+++ Structure_While_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Digite um número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while(x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite um outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Número Negativo!");
+        }
+    }
+}
---
+++ Structure_While_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EstruturaWhile01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida!");
+                Console.Write("Digite novamente a senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Parmitido!");
+        }
+    }
+}
