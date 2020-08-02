b660ca3c3fcc174b48881a6327ee8fd4f88f9afe
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:07:22 -0400

    Function calculator REF and OUT

---
+++ Function_Calculator_Ref_E_Out/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionCalculatorRefEOut {
+    class Calculator {
+        //=============================================================================
+        //                            IMPLEMENTANDO REF
+        //=============================================================================
+        public static void TripleRef(ref int x) { // ref é o modificador de parametro.
+            x = x * 3;
+        }
+        //=============================================================================
+        //                            IMPLEMENTANDO OUT
+        //=============================================================================
+        public static void TripleOut(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Function_Calculator_Ref_E_Out/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FunctionCalculatorRefEOut {
+    class Program {
+        static void Main(string[] args) {
+
+            // ambos são considerados "CODE SMELLS"
+
+            //=============================================================================
+            //                            IMPLEMENTANDO REF
+            //=============================================================================
+
+            // A variavel passada como parâmetro ref DEVE ter sido iniciada
+            
+            int a = 10; // usando Ref é obrigado a iniciar uma variavel com um valor.
+            Calculator.TripleRef(ref a);  // ref é o modificador de parametro. Se não usar
+                                          // o Ref não traz o valor do calculo.
+            Console.WriteLine(a);
+
+            //=============================================================================
+            //                            IMPLEMENTANDO OUT
+            //=============================================================================
+
+            //A variável passada como parêmetro out não precisa ter sido iniciada.
+
+            a = 10;
+            int triple;
+            Calculator.TripleOut(a, out triple);  //triple vai ser uma variavel de saida para
+                                                  //não perder o valor quando voltar da função.
+            Console.WriteLine(triple);
+
+
+        }
+    }
+}
