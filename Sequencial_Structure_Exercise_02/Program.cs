e699fe080001ef5a461ad78b482f644c137fdaa4
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:41:36 -0400

    Sequencial structure exercises

---
+++ Sequencial_Structure_Exercise_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaSequencialExercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = pi * (Math.Pow(raio, 2.0));
+
+            Console.WriteLine("A = " + resultado.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
