65df16ff88b1841615e7493354624e990ff3d137
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:09:04 -0400

    Example of Employees Control

---
+++ Employees_Control_Example/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlEmployees {
+    class Employee {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public double salary { get; set; }
+
+        public void increaseSalary(double percentage) {
+            salary += salary * percentage / 100.00;
+        }
+
+    }
+}
