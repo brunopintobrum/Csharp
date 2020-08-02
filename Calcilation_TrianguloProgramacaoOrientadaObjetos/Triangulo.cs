f713a6fac197eff797347514c08063bd32ea56ae
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:51:52 -0400

    Calculation of triangle - Object oriented Programing exercices

---
+++ Calcilation_TrianguloProgramacaoOrientadaObjetos/Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CalculoTrianguloProgramacaoOrientadaObjetos
+{
+    class Triangulo
+    {
+        //atributos da classe
+        public double A;
+        public double B;
+        public double C;
+
+        //metodos da classe
+        public double AreaTringulo()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
