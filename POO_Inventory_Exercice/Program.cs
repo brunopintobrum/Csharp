71cff96cc65a44241923928153476ecfca15448d
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:02:06 -0400

    POO - example invetory

---
+++ POO_Inventory_Exercice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace POOEstoqueExercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine(p);
+
+
+        }
+    }
+}
