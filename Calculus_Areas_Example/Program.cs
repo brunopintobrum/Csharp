c084d50dbcdb92f7977e90f07309f4b34619d4e8
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:10:24 -0400

    Calculus of areas example

---
+++ Calculus_Areas_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreasCalculos {
+    class Program {
+        static void Main(string[] args) {
+
+            double A, B, C,TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO, pi = 3.14159;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            TRIANGULO = A * C / 2;
+            CIRCULO = pi * Math.Pow(C, 2);
+            TRAPEZIO = (A + B) * C / 2;
+
+
+
+            Console.WriteLine("TRIANGULO: " + TRIANGULO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + CIRCULO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + TRAPEZIO.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
