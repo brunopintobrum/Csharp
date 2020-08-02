981aefc8ea720b806d50350d7fd58343a8efc932
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:06:49 -0400

    Function calculator params

---
+++ Function_Calculator_Params/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionCalculator {
+    class Calculator {
+        // ========================================================
+        //               FORMA ERRADA DE SER FEITO
+        // ========================================================
+        //public static int sum(int n1, int n2) {
+        //    return n1 + n2;
+        //}
+        //public static int sum(int n1, int n2, int n3) {
+        //    return n1 + n2 + n3;
+        //}
+        //public static int sum(int n1, int n2, int n3, int n4) {
+        //    return n1 + n2 + n3 + n4;
+        //}
+
+        // ========================================================
+        //        FORMA CORRETA DE SER FEITO - COM VETOR
+        // ========================================================
+        public static int sum(params int[] numbers) {
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        // ========================================================
+    }
+}
---
+++ Function_Calculator_Params/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FunctionCalculator {
+    class Program {
+        static void Main(string[] args) {
+            // ========================================================
+            //               FORMA ERRADA DE SER FEITO
+            // ========================================================
+
+            //int s1 = Calculator.sum(2, 3);
+            //int s2 = Calculator.sum(2, 4, 3);
+
+            //Console.WriteLine(s1);
+            //Console.WriteLine(s2);
+
+            // ========================================================
+            //        FORMA CORRETA DE SER FEITO - COM VETOR
+            // ========================================================
+
+            int s1 = Calculator.sum(2, 3);
+            int s2 = Calculator.sum(2, 3, 4);
+            int s3 = Calculator.sum(10, 10, 10, 10, 10);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+
+
+        }
+    }
+}
