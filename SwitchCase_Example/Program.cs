79d4648fe6623e2f0e5e5d0e6d39e93a45c650fd
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:14:48 -0400

    SwitchCase Example

---
+++ SwitchCase_Example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SwitchCase01 {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x) {
+                case 1:
+                    day = "Domingo";
+                    break;
+                case 2:
+                    day = "Segunda";
+                    break;
+                case 3:
+                    day = "Terça";
+                    break;
+                case 4:
+                    day = "Quarta";
+                    break;
+                case 5:
+                    day = "Quinta";
+                    break;
+                case 6:
+                    day = "Sexta";
+                    break;
+                case 7:
+                    day = "Sábado";
+                    break;
+                default:
+                    day = "Invalid value!";
+                    break;
+            }
+            Console.WriteLine("Dia: " + day);
+        }
+    }
+}
