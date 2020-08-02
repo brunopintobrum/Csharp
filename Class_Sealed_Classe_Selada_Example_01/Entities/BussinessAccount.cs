89d46f1ca1867b040e9ae2e4aab74e42e5012677
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:27:29 -0400

    Class sealed - Classe selada

---
+++ Class_Sealed_Classe_Selada_Example_01/Entities/BussinessAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_Example_02.Entities
+{
+    class BussinessAccount : Account
+    {
+        public double loanLimit { get; set; }
+
+        public BussinessAccount()
+        {
+
+        }
+
+        public BussinessAccount(int number, string holder, double balance, double loanLimit): base(number, holder, balance)
+        {
+            this.loanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= loanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Class_Sealed_Classe_Selada_Example_01/Entities/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_Example_02.Entities
+{
+    sealed class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; } //taxa de juros
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            this.InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount) //nao pode ser sobreescrita em uma outra subclasse, está selado o metodo.
+        {
+            base.Withdraw(amount); //chamada da regra da classe mae
+            Balance -= 2.0;
+        }
+    }
+}
