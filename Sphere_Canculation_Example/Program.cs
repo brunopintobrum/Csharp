f77c3e56b1a9372d7dbab6043e312f4e3340b1fd
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:09:48 -0400

    Sphere calculation example

---
+++ Sphere_Canculation_Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace EsferaCalculo {
+    class Program {
+        static void Main(string[] args) {
+
+            double volume, pi = 3.14159;
+
+            int raio = int.Parse(Console.ReadLine());
+            
+            volume = (4 / 3.0) * pi * Math.Pow(raio,3);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
