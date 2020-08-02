87c566bc844a7e06d01e3019d137f9b8f76564c4
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:05:35 -0400

    Examples using Vectors

---
+++ Vector_Example_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[numero];
+
+            for(int i = 0; i < numero; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0;
+            for(int i = 0; i < numero; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / numero;
+
+            Console.WriteLine("Average Height = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
