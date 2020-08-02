4785283adfa0bc857d8f4757211bd6a653497125
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:00:23 -0400

    Example Currency conversion

---
+++ POO_Currency_Conversion/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POOConversaoDolar
+{
+    class ConversorDeMoeda
+    {
+
+        public static double iof = 6.38;
+
+        public static double DolarParaReal(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * iof / 100.0;
+        }
+
+
+    }
+}
---
+++ POO_Currency_Conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace POOConversaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar Canadense? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine("O valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
