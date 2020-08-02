58178be670eef7218f7f5a9709a4f90887fd9838
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:14:17 -0400

    Type inference example

---
+++ Type_Inference_Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InferenciaDeTipos01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var x = 10;
+            var y = 20.0;
+            var z = "Bruno";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+
+
+
+        }
+    }
+}
