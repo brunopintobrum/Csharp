288959ea46bd081e1eb9b2d9df6225afe33306d6
Bruno Brum <brunobrum@gmail.com>
2020-08-02 14:18:35 -0400

    Example using DateTime, properties, ISO8601, TimeSpan, DateTimeKind

---
+++ DateTime_Example_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DateTime01 {
+    class Program {
+        static void Main(string[] args) {
+
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);// representa a data e hora, é armazenado assim, um numero inteiro.
+            Console.WriteLine("============================================================");
+            Console.WriteLine();
+
+            DateTime d2 = new DateTime(2018, 11, 25);
+            Console.WriteLine(d2);
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3);
+            Console.WriteLine(d3);
+            DateTime d4 = new DateTime(2018, 11, 25, 20, 45, 3, 500);
+            Console.WriteLine(d4);
+
+            Console.WriteLine("============================================================");
+            Console.WriteLine();
+
+            DateTime d5 = DateTime.Today;
+            Console.WriteLine(d5);
+            DateTime d6 = DateTime.UtcNow;
+            Console.WriteLine(d6);
+        }
+    }
+}
---
+++ TimeSpan_Example/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TimeSpan01 {
+    class Program {
+        static void Main(string[] args) {
+
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            TimeSpan t2 = new TimeSpan(); //construtor padrão.
+            Console.WriteLine(t2);
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            TimeSpan t3 = new TimeSpan(900000000L); // passa o ticks
+            Console.WriteLine(t3);
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            TimeSpan t4 = new TimeSpan(2, 11, 21);
+            Console.WriteLine(t4);
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21);
+            Console.WriteLine(t5);
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            TimeSpan t6 = new TimeSpan(1, 2, 11, 21, 321);
+            Console.WriteLine(t6);
+
+            Console.WriteLine();
+            Console.WriteLine("================================");
+
+            TimeSpan t7 = TimeSpan.FromDays(1.5);
+            Console.WriteLine(t7);
+            TimeSpan t8 = TimeSpan.FromHours(1.5);
+            Console.WriteLine(t8);
+            TimeSpan t9 = TimeSpan.FromMinutes(1.5);
+            Console.WriteLine(t9);
+            TimeSpan t10 = TimeSpan.FromSeconds(1.5);
+            Console.WriteLine(t10);
+            TimeSpan t11 = TimeSpan.FromMilliseconds(1.5);
+            Console.WriteLine(t11);
+
+
+
+
+        }
+    }
+}
