4c4a9024015c6749bd22e772a74b784e460e08a4
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:29:29 -0400

    My first project in C#

---
+++ Premier_Projet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Premier_Projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
