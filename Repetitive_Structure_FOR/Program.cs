77430019af7c110b71faef01de1ec8d3f7a10ad1
Bruno Brum <brunobrum@gmail.com>
2020-08-02 13:46:50 -0400

    Repetitive structure FOR

---
+++ Repetitive_Structure_FOR/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaRepetitivaFOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiro você irá digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+
+            }
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
