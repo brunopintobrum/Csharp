d53d4d016aa352cc60462cafca58061835116c87
Bruno Brum <brunobrum@gmail.com>
2020-08-08 16:25:29 -0400

    Exercice and Example - using TRY and CATCH and FINALLY - good solutions

---
+++ Exception_Try_Catch_Example_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exception_Try_Catch_Example_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                //Console.WriteLine("Error! " + e.Message);
+                Console.WriteLine("Division by zero is not allowed!");
+            } 
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);              
+            }
+        }
+    }
+}
---
+++ Exception_Try_Catch_Finally_Exercice_02/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exception_Try_Catch_Finally_Exercice_02.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
