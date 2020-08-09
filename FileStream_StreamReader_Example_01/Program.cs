fa5b996068aa798a05a7a4cce3c6767c6bd4bd44
Bruno Brum <brunobrum@gmail.com>
2020-08-08 20:20:26 -0400

    Example - Using FileStream StreamReader

---
+++ FileStream_StreamReader_Example_01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace FileStream_StreamReader_Example_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"F:\OneDrive\Cursos Udemy\C# COMPLETO 2020\FileStream_StreamReader_Example_01\temp\file01.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                //while (!sr.EndOfStream) *** imprime todas as linhas
+                //{
+                //    string line = sr.ReadLine();
+                //    Console.WriteLine(line);
+                //}
+                string line = sr.ReadLine(); //imprime só a primeira linha
+                Console.WriteLine(line);
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
--- File_FileInfo_IOException_Example_01/Program.cs
+++ File_FileInfo_IOException_Example_01/Program.cs
@@ -7,20 +7,18 @@ namespace File_FileInfo_IOException_Example_01
     {
         static void Main(string[] args)
         {
-
             string sourcePath = @"F:\OneDrive\Cursos Udemy\C# COMPLETO 2020\File_FileInfo_IOException_Example_01\temp\file1.txt";
             string targetPath = @"F:\OneDrive\Cursos Udemy\C# COMPLETO 2020\File_FileInfo_IOException_Example_01\temp\file2.txt";
 
             try
             {
                 FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
+                fileInfo.CopyTo(targetPath); //
 
                 string[] lines = File.ReadAllLines(sourcePath);
                 foreach (string line in lines)
                 {
                     Console.WriteLine(line);
-
                 }
             }
             catch (IOException e)
@@ -28,11 +26,6 @@ namespace File_FileInfo_IOException_Example_01
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
-
-
-
-
-
         }
     }
 }
