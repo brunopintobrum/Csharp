0f3494ec3dc02e6dbdf2e0b4ccdf4791e2898e2b
Bruno Brum <brunobrum@gmail.com>
2020-08-09 13:32:05 -0400

    Example using StreamWriter

---
+++ StreamWriter_Example01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriter_Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"F:\OneDrive\Cursos Udemy\C# COMPLETO 2020\StreamWriter_Example01\temp\file01.txt";
+            string targetPath = @"F:\OneDrive\Cursos Udemy\C# COMPLETO 2020\StreamWriter_Example01\temp\file02.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
